fe22da1fccd72de08f2aeade31f3add3bf4d0214
zhixin <wenzhixin2010@gmail.com>
2013-08-16 10:32:08 +0800

    fix: trigger link click error

--- js/login.js
+++ js/login.js
@@ -3,13 +3,13 @@
  * @date 2013-02-28
  */
 $(function() {
-	var WAIT_TIME = 500;
+	var WAIT_TIME = 800;
 
 	function main() {
 		var $iframe = $('#lhgfrm_lhgdgId').contents();
 		if ($iframe.length > 0) {
 			$('#trialRadio', $iframe).click();
-			$('#continue', $iframe).click();
+			triggerClick($('#continue', $iframe));
 		}
 		
 		var $form1 = $('#form1');
@@ -18,13 +18,21 @@ $(function() {
 				if (response.user) {
 					$('#UserNameID', $form1).val(response.user.username);
 					$('#PWID', $form1).val(response.user.password);
-					$('#LoginButton', $form1).click();
+					triggerClick($('#LoginButton', $form1));
 				}
 			});
 		}
 	}
 	
+	// fix: trigger link click
+	function triggerClick($el) {
+		var evt = document.createEvent("MouseEvents");
+        evt.initEvent("click", false, false);
+		$el.get(0).dispatchEvent(evt);
+	}
+	
 	chrome.extension.sendRequest({method: "getAutoLogin"}, function(response) {
 		if (response.result) setTimeout(main, WAIT_TIME);
 	});
+	
 });
